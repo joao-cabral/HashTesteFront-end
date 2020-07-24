f913a8f4f7f108425984d04a0ccd280a2f7683a1
Joao Cabral <joaocabral6665@gmail.com>
2020-07-23 22:15:20 -0400

    Connection in the api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 import '../styles/styles.scss'
+import axios from 'axios'
+
+const data = {
+   "amount": 15000,
+	"installments": 3,
+	"mdr": 4
+}
+
+const api = axios.create({
+   baseURL: 'https://hash-front-test.herokuapp.com/',
+})
+
+async function connect(){
+   try{
+      const requestedDatas = await api.post( 'https://hash-front-test.herokuapp.com/', data, {
+         headers: {
+            header: 'content-type: application/json',
+         }
+      })
+
+      console.log(requestedDatas.data)
+
+   }catch(err){
+      alert('something wrong happened')
+   }
+}
+
+connect()
